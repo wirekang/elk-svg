495ef3c48a498f444f2fc254e6e8e3407f0d7eda
wirekang <wirekang@gmail.com>
2024-05-23 23:59:55 +0900

    feat: add input types

--- src/elk-svg.ts
+++ src/elk-svg.ts
@@ -1,10 +1,10 @@
+import type { ElkSvgInputNode } from "./input-types";
 import { defaultLogger, type Logger } from "./logger";
 import { Preprocessor } from "./preprocess";
 import { Renderer } from "./render";
 import type { DefaultRenderingOptions } from "./rendering-types";
-import type { Shape } from "./shape-types";
 import { defaultShapes } from "./shape";
-import { ElkSvgStrictNode } from "./strict-types";
+import type { Shape } from "./shape-types";
 
 export class ElkSvg {
   private readonly renderer: Renderer;
@@ -29,13 +29,8 @@ export class ElkSvg {
     this.preprocessor = new Preprocessor(o.defaultRenderingOptions, o.shapes);
   }
 
-  /**
-   * By default, type checking for input node is disabled not to bother typesystem.
-   *
-   * Use {@link ElkSvgStrictNode} If you need a strict type (e.g. writing literal node).
-   */
-  public render(node: any) {
-    const flatElements = this.preprocessor.start(node);
+  public render(node: ElkSvgInputNode) {
+    const flatElements = this.preprocessor.start(node as any);
     this.renderer.render(flatElements);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./elk-svg";
 export * from "./rendering-types";
 export * from "./strict-types";
 export * from "./shape-types";
+export * from "./input-types";
